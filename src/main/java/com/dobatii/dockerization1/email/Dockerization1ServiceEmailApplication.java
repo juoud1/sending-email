590dbe3d33cf00897d955377ba4764f168274373
Dongongo <juoud1@gmail.com>
2019-12-18 06:00:26 -0500

    Configuring dummy JavaMailSender with spring boot

---
+++ src/main/java/com/dobatii/dockerization1/email/Dockerization1ServiceEmailApplication.java
@@ -0,0 +1,13 @@
+package com.dobatii.dockerization1.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dockerization1ServiceEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dockerization1ServiceEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dobatii/dockerization1/email/Dockerization1ServiceEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobatii.dockerization1.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Dockerization1ServiceEmailApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
